f89032f09481bd7fbb1d12d3f82e8f288fee1322
Matheesha <pathiranamatheesha@gmail.com>
2022-02-27 08:47:07 +0530

    update

--- main.py
+++ main.py
@@ -3,19 +3,25 @@ from pypresence import *
 import time
 from colored import fg, bg, attr
 
-rpc = Presence("") #your application client ID
+rpc = Presence("944039007258554379")
 rpc.connect()
 print("%sZoom Rich presence has started! Created by matheesha#2006%s" % (fg(45), attr(0)))
 
 running = False
 
+def default():
+    print("%sZoom has started, connecting Rich Presence!%s" % (fg(46), attr(0)))
+    rpc.update(state="In a Zoom Meeting", large_image="zoom_meeting",large_text="Zoom Meetings",small_image="red_dot",small_text="In a Zoom Meeting", start=time.time())
+
 def run_rpc():
     print("%sZoom has started, connecting Rich Presence!%s" % (fg(46), attr(0)))
-    rpc.update(state="In a Zoom Meeting", large_image="zoom_meeting", start=time.time())
+    rpc.update(state="In a Zoom Meeting", large_image="zoom_meeting",large_text="Zoom Meetings",small_image="red_dot",small_text="In a Zoom Meeting",details=(topic), start=time.time())
+
 
 def dc_rpc():
     print("%sZoom has been closed, disconnecting Rich Presence...%s" % (fg(1), attr(0)))
     rpc.clear()
+
 
 def process_exists(process_name):
     call = 'TASKLIST', '/FI', 'imagename eq %s' % process_name
@@ -27,11 +33,17 @@ def process_exists(process_name):
 while True:
     if process_exists("Zoom.exe") == True:
         if running == False:
-            run_rpc()
+            topic = input("Please Enter Your Zoom meeting Topic (To skip type 'x') :")
+            if topic == 'x':
+                default()
+            else:
+                run_rpc()
+
             running = True
-    
-    else: 
+
+    else:
         if running == True:
             dc_rpc()
             running = False
     time.sleep(1)
+
